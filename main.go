7dd7c3d53021148b8e84082accbd3b6d6ce51963
Airton Ponce <airtonponce@outlook.com>
2020-07-24 08:59:24 -0300

    newCard function with return type

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 import "fmt"
 
-var third_card string
-var forth_card string = "Queen of Clubs"
-
 func main() {
   fmt.Println("Shuffle up and deal!")
 
-  var card string = "Ace of Spades"
-  second_card := "Ace of Diamonds"
-  third_card = "Queen of Hearts"
+  card := newCard()
   fmt.Println(card)
-  fmt.Println(second_card)
-  fmt.Println(third_card)
-  fmt.Println(forth_card)
+}
+
+func newCard() string {
+  return "Five of Diamonds"
 }
