5af85dd8707d783d9e636b484e3cd66d0434c91e
Airton Ponce <ponceairton@gmail.com>
2020-07-25 08:18:31 -0300

    adjust identation

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
+
 import "fmt"
 
 func main() {
-  fmt.Println("Shuffle up and deal!")
+	fmt.Println("Shuffle up and deal!")
 
-  cards:= []string{newCard(),"Ace of Diamonds"}
-  cards = append(cards, "Six of Spades")
-  fmt.Println(cards)
-  var cardsArray[3]int =[3]int{3,2,1}
-  fmt.Println(cardsArray)
+	cards := []string{newCard(), "Ace of Diamonds"}
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+	var cardsArray [3]int = [3]int{3, 2, 1}
+	fmt.Println(cardsArray)
 
-  for i, card := range cardsArray {
-    fmt.Println(i, card)
-  }
+	for index, card := range cardsArray {
+		fmt.Println(index, card)
+	}
 }
 
 func newCard() string {
-  return "Five of Diamonds"
+	return "Five of Diamonds"
 }
